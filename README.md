3ffa430b8909a8481235d328555567c334762379
Unjustifiable1 <cupidojustin7@gmail.com>
2024-04-08 22:11:25 +0200

    footer social icons and links setup. contact detail links added

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Red Cape Intel website as landing page and point of contact for client leads.
 
 Logo created with Logomakr.com
 app.logomakr.com/5jXTaj
+
+<a href="https://www.freepik.com/free-vector/social-media-logo-collection_4031753.htm#from_view=detail_alsolike">Image by freepik</a>
--- mainscript.js
+++ mainscript.js
@@ -22,9 +22,9 @@ function scrollFunction() {
   } else if (window.innerWidth > 400) {
     document.getElementById("navLogoContainer").style.height = "4em";
     document.getElementById("navCTA").style.fontSize = "1em";
-  } else if (window.innerWidth > 250) {
-    document.getElementById("navLogoContainer").style.height = "3em";
-    document.getElementById("navCTA").style.fontSize = "1em";
+  // } else if (window.innerWidth > 250) {
+  //   document.getElementById("navLogoContainer").style.height = "3em";
+  //   document.getElementById("navCTA").style.fontSize = "1em";
   } else return;
 }
 
